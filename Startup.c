f13bd4a0fdf24e322bd25a436472a29c54ce7504
ZE-FRE <1757943893@qq.com>
2024-07-05 09:27:32 +0000

    update Startup.c.
    Signed-off-by: ZE-FRE <1757943893@qq.com>

--- Startup.c
+++ Startup.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include "BstTree.h"
+#include "Stack.h"
 
-int main(int argc, char* argv[]) 
+int main(int argc, char* argv[])
 {
+	//testStack();
+
 	testBstTree();
 
 	return 0;
